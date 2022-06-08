ca73c74231125e8a8e50f5a96f5f18da46ef3406
ducdaudshp <ducdaudshp@gmail.com>
2022-06-08 15:10:41 +0700

    them truong dia chi cho house

--- src/main/java/com/duong/casemodule6/controller/HouseController.java
+++ src/main/java/com/duong/casemodule6/controller/HouseController.java
@@ -1,10 +1,15 @@
 package com.duong.casemodule6.controller;
 
+import com.duong.casemodule6.entity.dto.HouseForm;
+import com.duong.casemodule6.entity.house.House;
+import com.duong.casemodule6.entity.house.Room;
+import com.duong.casemodule6.service.house.IHouseService;
 import com.duong.casemodule6.service.room.IRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/house")
@@ -13,5 +18,31 @@ public class HouseController {
 
     @Autowired
     private IRoomService roomService;
+
+    @Autowired
+    private IHouseService houseService;
+
+    @Autowired
+    Environment env;
+
+    @ModelAttribute("rooms")
+    private Iterable<Room> rooms(){
+        return roomService.findAll();
+    }
+
+    @GetMapping("/room")
+    public ResponseEntity<Iterable<Room>> showAllRoom(){
+        return new ResponseEntity<>(roomService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Iterable<House>> showAllHouse(){
+        return new ResponseEntity<>(houseService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<House> createHouse(@ModelAttribute HouseForm houseForm){
+
+    }
 
 }
