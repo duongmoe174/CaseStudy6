3ff8b6ccfa78812d9bcb8b6b4271b3cc34fd4257
ducdaudshp <ducdaudshp@gmail.com>
2022-06-08 17:12:46 +0700

    crud house

--- src/main/java/com/duong/casemodule6/controller/HouseController.java
+++ src/main/java/com/duong/casemodule6/controller/HouseController.java
@@ -3,16 +3,25 @@ package com.duong.casemodule6.controller;
 import com.duong.casemodule6.entity.dto.HouseForm;
 import com.duong.casemodule6.entity.house.House;
 import com.duong.casemodule6.entity.house.Room;
+import com.duong.casemodule6.entity.user.Host;
+import com.duong.casemodule6.service.host.IHostService;
 import com.duong.casemodule6.service.house.IHouseService;
 import com.duong.casemodule6.service.room.IRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
 
 @RestController
-@RequestMapping("/house")
+@RequestMapping("/houses")
 @CrossOrigin("*")
 public class HouseController {
 
@@ -24,6 +33,9 @@ public class HouseController {
 
     @Autowired
     Environment env;
+
+    @Autowired
+    private IHostService hostService;
 
     @ModelAttribute("rooms")
     private Iterable<Room> rooms(){
@@ -42,7 +54,75 @@ public class HouseController {
 
     @PostMapping
     public ResponseEntity<House> createHouse(@ModelAttribute HouseForm houseForm){
+        MultipartFile multipartFile = houseForm.getImage();
+        String fileName = multipartFile.getOriginalFilename();
+        String fileUpLoad = env.getProperty("upload.path").toString();
+        String name = houseForm.getName();
+        Set<Room> room_category = houseForm.getRoom_category();
+        String address = houseForm.getAddress();
+        int numberOfBedroom = houseForm.getNumberOfBedroom();
+        int numberOfBathroom = houseForm.getNumberOfBathroom();
+        String description = houseForm.getDescription();
+        double price = houseForm.getPrice();
+        Boolean status = houseForm.getStatus();
+        Host host = houseForm.getHost();
+        try{
+            FileCopyUtils.copy(multipartFile.getBytes(), new File(fileUpLoad+fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        House house = new House(name,room_category,address,numberOfBedroom,numberOfBathroom,description,price,fileName,status,host);
+        houseService.save(house);
+        return new ResponseEntity<>(house, HttpStatus.CREATED);
+    }
 
+    @PostMapping("/{id}")
+    public ResponseEntity<House> editHouse(@PathVariable Long id, @ModelAttribute HouseForm houseForm){
+        Optional<House> houseOptional = houseService.findById(id);
+        if (!houseOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            MultipartFile multipartFile = houseForm.getImage();
+            String fileName = multipartFile.getOriginalFilename();
+            String fileUpLoad = env.getProperty("upload.path").toString();
+            String name = houseForm.getName();
+            Set<Room> room_category = houseForm.getRoom_category();
+            String address = houseForm.getAddress();
+            int numberOfBedroom = houseForm.getNumberOfBedroom();
+            int numberOfBathroom = houseForm.getNumberOfBathroom();
+            String description = houseForm.getDescription();
+            double price = houseForm.getPrice();
+            Boolean status = houseForm.getStatus();
+            Host host = houseForm.getHost();
+            try{
+                FileCopyUtils.copy(houseForm.getImage().getBytes(), new File(fileUpLoad+fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            House house = new House(name,room_category,address,numberOfBedroom,numberOfBathroom,description,price,fileName,status,host);
+            house.setId(id);
+            houseService.save(house);
+            return new ResponseEntity<>(house, HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<House> deleteHouse (@PathVariable Long id){
+        Optional<House> houseOptional = houseService.findById(id);
+        if (!houseOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        houseService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<House> findOne(@PathVariable Long id){
+        Optional<House> houseOptional = houseService.findById(id);
+        if (!houseOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(houseOptional.get(), HttpStatus.OK);
     }
 
 }
